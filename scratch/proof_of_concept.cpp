4799cdd1fcb04e8d9be1d8d8a78c3ece1827710d
David Kennedy S. Araújo <software@davidkennedy.dev>
2025-01-02 16:54:48 -0300

    PoC to visit each base class instance

--- scratch/proof_of_concept.cpp
+++ scratch/proof_of_concept.cpp
@@ -11,16 +11,16 @@ concept VisitWithNameAnd =
     };
 
 template <typename Callable, typename... FieldType>
-concept FieldVisitor =
-    (... && VisitWithNameAnd<Callable, FieldType>);
+concept Visitor = (... && VisitWithNameAnd<Callable, FieldType>);
 
 template <typename T> class Introspect {
 public:
-  constexpr void foreachField(FieldVisitor auto &&apply);
+  constexpr void foreachField(Visitor auto &&apply);
+  constexpr void foreachBaseClass(Visitor auto &&apply);
 };
 
 template <typename UserType>
-Introspect<UserType> introspecto(UserType& instance) {
+Introspect<UserType> introspecto(UserType &instance) {
   return Introspect<UserType>(instance);
 }
 
@@ -31,6 +31,10 @@ struct Person {
   std::string fullname;
 };
 
+struct PersonWithExtra : public Person {
+  double extra;
+};
+
 // Generated
 
 template <> class Introspect<Person> {
@@ -39,9 +43,26 @@ template <> class Introspect<Person> {
 public:
   constexpr Introspect(Person &instance) : instance(instance) {}
 
-  constexpr void foreachField(FieldVisitor auto &&apply) {
+  constexpr void foreachField(Visitor auto &&apply) {
     apply("age", instance.age);
     apply("fullname", instance.fullname);
+  }
+
+  constexpr void foreachBaseClass(Visitor auto &&apply) {}
+};
+
+template <> class Introspect<PersonWithExtra> {
+  PersonWithExtra &instance;
+
+public:
+  constexpr Introspect(PersonWithExtra &instance) : instance(instance) {}
+
+  constexpr void foreachField(Visitor auto &&apply) {
+    apply("extra", instance.extra);
+  }
+
+  constexpr void foreachBaseClass(Visitor auto &&apply) {
+    apply("Person", static_cast<Person>(instance));
   }
 };
 
@@ -53,9 +74,31 @@ int main() {
 
   Person person{18, "ze zim"};
 
-  auto personInfo = introspecto(person);
+  auto person_info = introspecto(person);
 
-  personInfo.foreachField(
+  person_info.foreachField([](const std::string_view name, const auto value) {
+    std::cout << value << std::endl;
+  });
+
+  std::cout << "---" << std::endl;
+
+  PersonWithExtra person_with_extra{10, "fulano", 42.00};
+
+  auto person_info_with_extra = introspecto(person_with_extra);
+
+  person_info_with_extra.foreachBaseClass(
+      [](const std::string_view name, auto base_instance) {
+        std::cout << name << ":\n";
+
+        auto base_instance_info = introspecto(base_instance);
+
+        base_instance_info.foreachField(
+            [](const std::string_view name, auto value) {
+              std::cout << "  " << value << std::endl;
+            });
+      });
+
+  person_info_with_extra.foreachField(
       [](const std::string_view name, const auto value) {
         std::cout << value << std::endl;
       });
